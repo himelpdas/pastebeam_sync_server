6a90ec8b2f92891f4dafacc910927cf82fd8efab
Himel Das (flip) <himel.p.das@gmail.com>
2015-10-06 14:28:58 -0400

    reformat

--- zmq_forwarder.py
+++ zmq_forwarder.py
@@ -1,18 +1,18 @@
 __author__ = 'Himel'
-
-#http://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/devices/forwarder.html
 from gevent import monkey; monkey.patch_all()
-import zmq.green as zmq
+import zmq.green as zmq  #http://learning-0mq-with-pyzmq.readthedocs.org/en/latest/pyzmq/devices/forwarder.html
+
+
 
 def main():
-
     try:
         context = zmq.Context(1)
         # Socket facing clients
         frontend = context.socket(zmq.SUB)
         frontend.bind("tcp://*:8882")
 
-        frontend.setsockopt(zmq.SUBSCRIBE, "") #The current version of zmq supports filtering of messages based on topics at subscriber side. This is usually set via socketoption.
+        frontend.setsockopt(zmq.SUBSCRIBE,
+                            "")  # The current version of zmq supports filtering of messages based on topics at subscriber side. This is usually set via socketoption.
 
         # Socket facing services
         backend = context.socket(zmq.PUB)
@@ -27,6 +27,7 @@ def main():
         frontend.close()
         backend.close()
         context.term()
+
 
 if __name__ == "__main__":
     main()
