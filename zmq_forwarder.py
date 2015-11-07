62ee8740c9526efcdd3fe0a5785a8e3c6e1df751
Himel Das (Spectre-X360) <himel.p.das@gmail.com>
2015-11-07 06:04:42 -0500

    comments

--- zmq_forwarder.py
+++ zmq_forwarder.py
@@ -12,7 +12,7 @@ def main():
         frontend.bind("tcp://*:8882")
 
         frontend.setsockopt(zmq.SUBSCRIBE,
-                            "")  #"" means don't filter anything #Another important thing to notice is that we want all the published messages to reach to the various subscribers, hence message filtering should be off in the forwarder device. See line no 11.
+                            "")  #"" means subscribe to the socket, and don't filter anything (since this is a forwarder and we want all messages to pass # Another important thing to notice is that we want all the published messages to reach to the various subscribers, hence message filtering should be off in the forwarder device. See line no 11.
 
         # Socket facing services
         backend = context.socket(zmq.PUB) #as of zmq 3, message filtering is done here... in other words, zmq will only send it to the websocket subscriber listening for himeldas@live.com... Before it was the responsibility of the subscriber to filter, so the filtering would've been done in the websocket server, instead of here. This can get very bad for the websocket, if there are lots of messages
